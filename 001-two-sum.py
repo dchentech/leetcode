d8bd85229f308f4109b6186b1d798328ce9da4ab
David Chen <mvjome@gmail.com>
2015-10-02 17:14:37 +0800

    Update filenames

--- 001-two-sum.py
+++ 001-two-sum.py
@@ -1,15 +1,17 @@
 """
-Given an array of integers, find two numbers such that they add up to a specific target number.
+Question:
+    Given an array of integers, find two numbers such that they add up to a specific target number.
 
-The function twoSum should return indices of the two numbers such that they add up to the target, where index1 must be less than index2. Please note that your returned answers (both index1 and index2) are not zero-based.
+    The function twoSum should return indices of the two numbers such that they add up to the target, where index1 must be less than index2. Please note that your returned answers (both index1 and index2) are not zero-based.
 
-You may assume that each input would have exactly one solution.
+    You may assume that each input would have exactly one solution.
 
-Input: numbers={2, 7, 11, 15}, target=9
-Output: index1=1, index2=2
+    Input: numbers={2, 7, 11, 15}, target=9
+    Output: index1=1, index2=2
 
-Show Tags
-Show Similar Problems
+Annotation:
+    1. 2 times for loop statements are two slow.
+    2. Always remember that Cache is very important.
 """
 
 
