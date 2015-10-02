d7929892adfc783a30347b664f18087e8a231794
David Chen <mvjome@gmail.com>
2015-10-02 20:31:26 +0800

    Finally success

--- 003-longest-substring-without-repeating-characters.py
+++ 003-longest-substring-without-repeating-characters.py
@@ -3,10 +3,15 @@ Question:
     Longest Substring Without Repeating Characters My Submissions Question Solution
 
     Given a string, find the length of the longest substring without repeating characters. For example, the longest substring without repeating letters for "abcabcbb" is "abc", which the length is 3. For "bbbbb" the longest substring is "b", with the length of 1.
+
+
+Annotation:
+    1. In a O(1) algorithm, just start from the duplicated found element, and don't start with the window begin next element, cause it's already the longest substring without repeating characters until now.
+    2. performance, "Your runtime beats 37.55% of python submissions."
 """
 
 
-class Solution(object):
+class Solution_slow(object):
     def lengthOfLongestSubstring(self, s):
         """
         :type s: str
@@ -47,6 +52,35 @@ class Solution(object):
         print max_window
         return len(max_window.substring)
 
+
+class Solution(object):
+    def lengthOfLongestSubstring(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        # print "\ninput:", s
+        if len(s) == 0:
+            return 0
+
+        not_exists_idx = -1  # the index that we can't found an element in a Python array.
+        last_appeared_char_idxes = dict()
+        longest_length = 0
+        window_begin = 0
+        window_end = None
+
+        for idx, char in enumerate(s):
+            window_end = idx + 1
+            idx_pre = last_appeared_char_idxes.get(char, not_exists_idx)
+            if idx_pre != not_exists_idx:
+                window_begin = max(window_begin, idx_pre + 1)  # don't look back, cause there're more than two duplicated chars.
+            longest_length = max(longest_length, window_end - window_begin)
+            last_appeared_char_idxes[char] = idx
+            # print "idx:", idx, "longest_length:", longest_length, "window_begin:", window_begin, "window_end:", window_end
+
+        return longest_length
+
+
 result = Solution().lengthOfLongestSubstring("")
 assert result == len(""), result
 
@@ -58,3 +92,9 @@ assert result == len("abc"), result
 
 result = Solution().lengthOfLongestSubstring("bbbbb")
 assert result == len("b"), result
+
+result = Solution().lengthOfLongestSubstring("abbaaaaaaaa")  # double duplicated chars testcase.
+assert result == len("ab"), result
+
+result = Solution().lengthOfLongestSubstring("wobgrovw")
+assert result == len("bgrovw"), result
