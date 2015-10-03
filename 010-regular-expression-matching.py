04c649cac8a37a1cedf661dc3020b0eaa0323df0
David Chen <mvjome@gmail.com>
2015-10-03 16:55:41 +0800

    stucked at 010-regular-expression-matching.py, maybe solve it in future; other people's codes are very short, and they dont wrap code in class

--- 010-regular-expression-matching.py
+++ 010-regular-expression-matching.py
@@ -64,7 +64,8 @@ class PatternStaticMethods(object):
     def pattern_generator(input_string):
         for idx, re_char in enumerate(input_string):
             next_char = None if (idx + 1) == len(input_string) else input_string[idx + 1]
-            yield Pattern.new(re_char, next_char)
+            is_end = (idx + 1) == len(input_string)
+            yield Pattern.new(re_char, next_char), is_end
 
 
 class Pattern(PatternStaticMethods):
@@ -81,7 +82,7 @@ class Pattern(PatternStaticMethods):
 
     def process_single_char_common(self, entire_input, start_idx, yes=lambda: NotImplementedError):
         if not yes():
-            return CurrentStatus(False)
+            return CurrentStatus(False, start_idx)  # still the same start_idx
 
         next_idx = start_idx + 1
         should_next_re_char = True
@@ -106,18 +107,18 @@ class DotPattern(Pattern):
 
 class StarPattern(Pattern):
     def process(self, entire_input, start_idx):
-        if self.next_re_char is None:  # match the remain chars directly.
-            return CurrentStatus(True, start_idx + 1)
+        if self.next_re_char is None:  # * match the remain chars directly.
+            return CurrentStatus(True, start_idx)
 
         len_input = len(entire_input)
         end_idx = len_input - 1
         should_next_re_char = False
-        while start_idx <= end_idx:
-            if start_idx < end_idx:  # still have chars
-                if self.next_re_char == entire_input[start_idx + 1]:
-                    should_next_re_char = True
-                    break  # success & stop
-                # going on...
+        while start_idx <= end_idx:  # still have chars
+            # If match next char, and no need to increase below start_idx.
+            if (start_idx != end_idx) and (self.next_re_char == entire_input[start_idx + 1]):
+                should_next_re_char = True
+                break  # success & stop
+            # going on...
             start_idx += 1
 
         success = True
@@ -126,7 +127,7 @@ class StarPattern(Pattern):
             success = False
 
         if should_next_re_char:
-            return CurrentStatus(success, start_idx + 1, should_next_re_char)
+            return CurrentStatus(success, start_idx, should_next_re_char)
         else:
             return CurrentStatus(success, start_idx + 1)
 
@@ -140,44 +141,66 @@ class Solution(object):
         """
         p = Pattern.fix_regexp(p)
         start_idx = 0
-        is_valid = True
+        # is_success_in_the_end = True
+        current_status = None
+        is_reach_pattterns_end = False
 
-        print "\ninput: s:{}, p:{}".format(s, p)
-        for pattern in Pattern.pattern_generator(p):
-            print pattern
+        if len(s) == 0 and len(set(p) - set(["*", "."])) > 0:
+            return False
+        if len(p) == 0 and len(s) > 0:
+            return False
+        if len(p) == 0 and len(s) == 0:
+            return False
+
+        print "\n=====>>>>>>input: s:{}, p:{}".format(s, p)
+        for pattern, is_end in Pattern.pattern_generator(p):
+            print pattern, "|"*10
+            is_reach_pattterns_end = is_end
             status = pattern.process(s, start_idx)
+            current_status = status
             start_idx = status.next_idx  # update
             print status
+            print "  =>", " " * start_idx, s[start_idx:]
 
             # directly False
             if status.success is False:
-                # TODO go to next?
-                is_valid = False
-                break
+                continue  # go to next regexp pattern
             # need another pattern (but maybe there's no remain pattern)
             if status.success is True and status.should_next_re_char:
                 continue
             # yes, succesed
             if status.success is True and not status.should_next_re_char:
-                break
+                break  # even still has remain pattern
             raise ValueError("Should not reach here.")
 
-        # yet not finish input:s
+        print
+        if not is_reach_pattterns_end:
+            return False
+
+        # . full success
+        return current_status.success and \
+            (start_idx >= len(s))  # quick hack, but TODO == is greater
+
+        """
+        # 2. yet not finish input:s
         # why: cause pattern only care about itself, but not the total regexp.
         if (start_idx + 1) <= len(s):
-            is_valid = False
+            is_success_in_the_end = False
+        """
 
-        return is_valid
+        # return is_success_in_the_end
 
 
-"""
 assert Solution().isMatch("aa", "a") is False
 assert Solution().isMatch("aa", "aa") is True
 assert Solution().isMatch("aaa", "aa") is False
 assert Solution().isMatch("aa", "a*") is True
 assert Solution().isMatch("aa", ".*") is True
 assert Solution().isMatch("ab", ".*") is True
-"""
+assert Solution().isMatch("ab", ".*c") is False
 assert Solution().isMatch("aab", "c*a*b") is True
 assert Solution().isMatch("aab", "c**b") is True
-assert Solution().isMatch("aab", "*c") is False
+assert Solution().isMatch("", "c*") is False
+assert Solution().isMatch("a", "") is False
+assert Solution().isMatch("", "") is False
+# assert Solution().isMatch("aab", "*c") is False
