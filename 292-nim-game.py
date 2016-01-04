1ca2e05edb603797c2e1bffe57e3b4f37db5abcb
David Chen <mvjome@gmail.com>
2016-01-04 10:43:10 +0800

    Add 292-nim-game.py

---
+++ 292-nim-game.py
@@ -0,0 +1,45 @@
+"""
+Question:
+    Nim Game My Submissions Question
+
+    You are playing the following Nim Game with your friend: There is a heap of stones on the table, each time one of you take turns to remove 1 to 3 stones. The one who removes the last stone will be the winner. You will take the first turn to remove the stones.
+
+Both of you are very clever and have optimal strategies for the game. Write a function to determine whether you can win the game given the number of stones in the heap.
+
+For example, if there are 4 stones in the heap, then you will never win the game: no matter 1, 2, or 3 stones you remove, the last stone will always be removed by your friend.
+
+Hint:
+    If there are 5 stones in the heap, could you figure out a way to remove the stones such that you will always be the winner?
+    Credits:
+    Special thanks to @jianchao.li.fighter for adding this problem and creating all test cases.
+
+Performance:
+    1. Total Accepted: 31755 Total Submissions: 63076 Difficulty: Easy
+    2.  Your runtime beats 43.52% of python submissions.
+"""
+
+
+class Solution(object):
+    def canWinNim(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        if n <= 3:
+            return True
+
+        if n % 4 == 0:
+            return False
+        else:
+            return True
+
+
+assert Solution().canWinNim(0) is True
+assert Solution().canWinNim(1) is True
+assert Solution().canWinNim(2) is True
+assert Solution().canWinNim(3) is True
+assert Solution().canWinNim(4) is False
+assert Solution().canWinNim(5) is True
+assert Solution().canWinNim(6) is True
+assert Solution().canWinNim(7) is True
+assert Solution().canWinNim(8) is False
